899907ea74bc3845545fcb29b4cd0686b18d3fd8
Rajeevgangaraju <134952680+Rajeevgangaraju@users.noreply.github.com>
2026-06-16 15:08:33 +0530

    Create eks.tf

---
+++ terraform/eks.tf
@@ -0,0 +1,22 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+
+  cluster_endpoint_public_access = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size   = 1
+      max_size       = 2
+      min_size       = 1
+
+      instance_types = [var.instance_type]
+    }
+  }
+}
