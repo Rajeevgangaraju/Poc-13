a0bc92b0f57d629cbcb3de1f636c9d84c1bf3b38
Rajeevgangaraju <134952680+Rajeevgangaraju@users.noreply.github.com>
2026-06-16 15:08:05 +0530

    Create vpc.tf

---
+++ terraform/vpc.tf
@@ -0,0 +1,13 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "${var.cluster_name}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs            = ["ap-south-1a", "ap-south-1b"]
+  public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+
+  enable_nat_gateway = false
+  map_public_ip_on_launch = true
+}
