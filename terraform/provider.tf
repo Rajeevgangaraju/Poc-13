1c458f6821c3eea1837377cc304397dca5f7d770
Rajeevgangaraju <134952680+Rajeevgangaraju@users.noreply.github.com>
2026-06-16 15:05:58 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,12 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = var.region
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
